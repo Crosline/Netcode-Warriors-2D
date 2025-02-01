014a9af4e420fc82f6db6fb85a8a9ed00f7de357
Crosline <cagkancaglayanel@gmail.com>
2025-02-02 02:31:05 +0300

    Update: Canvas for host/join logic from UI

--- Assets/AAA/UI/Scripts/NetCodeConnectionButtons.cs
+++ Assets/AAA/UI/Scripts/NetCodeConnectionButtons.cs
@@ -1,25 +1,33 @@
-using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
-public class JoinClient : MonoBehaviour
+public class NetCodeConnectionButtons : MonoBehaviour
 {
+    [SerializeField]
     private Button _joinButton;
+    
+    [SerializeField]
+    private Button _hostButton;
     void Awake()
     {
-        _joinButton = GetComponent<Button>();
         _joinButton.onClick.AddListener(OnJoinButtonClicked);
+        _hostButton.onClick.AddListener(OnHostButtonClicked);
     }
 
     private void OnDestroy()
     {
         _joinButton.onClick.RemoveListener(OnJoinButtonClicked);
+        _hostButton.onClick.RemoveListener(OnHostButtonClicked);
     }
 
     private void OnJoinButtonClicked()
     {
         NetworkManager.Singleton.StartClient();
+    }
+    private void OnHostButtonClicked()
+    {
+        NetworkManager.Singleton.StartHost();
     }
 }
